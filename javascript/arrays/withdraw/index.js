aa619d50afaf588699344fb1027c3be20cb97362
kodershved <56606110+kodershved@users.noreply.github.com>
2022-10-26 12:04:31 +0300

    commit to main

--- javascript/arrays/withdraw/index.js
+++ javascript/arrays/withdraw/index.js
@@ -1,1 +1,15 @@
-function withdraw(clients, balances, client, amount) {}
+function withdraw(clients, balances, client, amount) {
+  let index = 0;
+  for (let i = 0; i < clients.length; i++) {
+    if (clients[i] === client) {
+      index = i;
+    }
+  }
+  if (balances[index] >= amount) {
+    balances[index] -= amount;
+    return balances[index];
+  }
+  return -1;
+}
+
+console.log(withdraw(["Ann", "John", "User"], [1400, 87, -6], "User", 10));
